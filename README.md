fccf2495ede9e10ba1c12f4bcd7aacd64ba3e2d7
hexenq <hexenq@gmail.com>
2018-07-05 10:50:53 +0800

    chore: update docs and modify ci

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # kuroshiro-analyzer-mecab
  
 [![Build Status](https://travis-ci.com/hexenq/kuroshiro-analyzer-mecab.svg?branch=master)](https://travis-ci.org/hexenq/kuroshiro-analyzer-mecab)
-[![Coverage Status](https://coveralls.io/repos/github/hexenq/kuroshiro-analyzer-mecab/badge.svg?branch=master)](https://coveralls.io/github/hexenq/kuroshiro-analyzer-mecab?branch=master)
 <!-- [![npm version](https://badge.fury.io/js/kuroshiro-analyzer-mecab.svg)](http://badge.fury.io/js/kuroshiro-analyzer-mecab)) -->
 
 <table>
@@ -16,7 +15,7 @@
     <tr>
         <td rowspan=2>Compatibility</td>
         <td>Node</td>
-        <td>✓</td>
+        <td>✓ (>=6)</td>
     </tr>
     <tr>
         <td>Browser</td>
--- src/index.js
+++ src/index.js
@@ -61,20 +61,6 @@ class Analyzer {
      * Parse the given string
      * @param {*} str input string.
      * @returns {Promise} Promise object represents the result of parsing.
-     * @example The standard of the output.
-     * [{
-     *     surface_form: '黒白',    // [Required] 表層形
-     *     pos: '名詞',               // 品詞 (Part Of Speech)
-     *     pos_detail_1: '一般',      // 品詞細分類1
-     *     pos_detail_2: '*',        // 品詞細分類2
-     *     pos_detail_3: '*',        // 品詞細分類3
-     *     conjugated_type: '*',     // 活用型
-     *     conjugated_form: '*',     // 活用形
-     *     basic_form: '黒白',      // 基本形
-     *     reading: 'クロシロ',       // [Required if japanese token] 読み
-     *     pronunciation: 'クロシロ',  // 発音
-     *     verbose: { }               // Other properties (Customized)
-     * }]
      */
     parse(str = "") {
         const parseToken = (token) => {
